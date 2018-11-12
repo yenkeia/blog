e08fc968d1e24e3a9257c60ac56c7b9340c63d08
wrq <pyq225@gmail.com>
2018-11-13 06:40:34 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // npm install node-github-webhook --save
 var http = require('http')
+var config = require('secret.json')
 var createHandler = require('node-github-webhook')
-var handler = createHandler({ path: '/bloghook', secret: 'appsecret' }) // single handler
+var handler = createHandler({ path: '/bloghook', secret: config.appSecret }) // single handler
 function execFunc (content) {
     var exec = require('child_process').exec
     exec(content, function (error, stdout, stderr) {
@@ -18,7 +19,7 @@ http.createServer(function (req, res) {
         res.statusCode = 404
         res.end('no such location')
     })
-}).listen(9001)
+}).listen(config.port)
 handler.on('error', function (err) {
     console.error('Error:', err.message)
 })

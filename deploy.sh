86be391cae32d5e0990702b19fed335d58ac659f
wrq <pyq225@gmail.com>
2018-11-15 23:47:54 +0800

    update theme to jane

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd /opt/blog
+# rm -rf /opt/blog/public
 git pull
+# mkdir -p /opt/blog/static/css
+# hugo gen chromastyles --style=trac > static/css/syntax.css
 # git submodule update --init --recursive
 git submodule update --recursive --remote
 hugo

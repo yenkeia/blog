aa67d518e9d279d3ad68f31816ab1a16011e0900
wrq <pyq225@gmail.com>
2018-11-16 00:00:53 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd /opt/blog
-# rm -rf /opt/blog/public
+git fetch --all
+git reset --hard origin/master
 git pull
+git submodule update --recursive --remote
+hugo
+
+# rm -rf /opt/blog/public
 # mkdir -p /opt/blog/static/css
 # hugo gen chromastyles --style=trac > static/css/syntax.css
 # git submodule update --init --recursive
-git submodule update --recursive --remote
-hugo
